ae83431f7276bb4b9b1176607f47c3e9c09ed30a
unknown <afyaakhudur@gmail.com>
2024-01-11 15:28:05 +0300

    turninig numbers to dots

--- src/Die.jsx
+++ src/Die.jsx
@@ -1,10 +1,39 @@
 import React from 'react'
 
 const Die = (props) => {
+
+  let dieFace ;
+
+  switch(props.value){
+    case 1:
+      dieFace = 'face-one';
+      break;
+    case 2:
+      dieFace = 'face-two';
+      break;
+    case 3:
+      dieFace = 'face-three';
+      break;
+    case 4:
+      dieFace = 'face-four';
+      break;
+    case 5:
+      dieFace = 'face-five';
+      break;
+    default:
+      dieFace = 'face-six'; 
+      break; 
+      
+  }
+
+  const dots =[]
+  for (let i = 1 ; i<= props.value ; i++){
+    dots.push(<span key={i} className='dot'></span>)
+  }
   return (
-    <div className='square' key={props.id} onClick={props.holdDice}
+    <div className={`square ${dieFace}`} key={props.id} onClick={props.holdDice}
      style={{ backgroundColor: props.isHeld? '#59E391' : 'white'}}>
-       <h1>{props.value}</h1> 
+       {dots}
     </div>
   )
 }
